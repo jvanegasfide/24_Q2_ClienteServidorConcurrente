0a700b8eac73ed7113d510be161a29e5c461a804
jumisingo <40045992+jumisingo@users.noreply.github.com>
2024-07-29 20:55:47 -0600

    Update script.sql

--- Sem11/script.sql
+++ Sem11/script.sql
@@ -7,5 +7,18 @@ mail VARCHAR(50) NULL,
 PRIMARY KEY (idprofesor)
 );
 
+#Insercion
 INSERT INTO universidad.profesores(nombre,mail)
-VALUES('Pedro Gonzalez', 'prueba@fidelitas.org')
+VALUES('Pedro Gonzalez', 'prueba@fidelitas.org');
+INSERT INTO universidad.profesores(nombre,mail)
+VALUES('Pedro Gonzalez', 'prueba@fidelitas.org');
+
+#Seleccion
+SELECT * FROM universidad.profesores where idprofesor = 1;
+
+#Actualizacion
+UPDATE profesores SET nombre = 'nombre', mail = 'correo' WHERE idprofesor = 2;
+
+#Borrado 
+DELETE FROM profesores WHERE idprofesor = 2;
+
